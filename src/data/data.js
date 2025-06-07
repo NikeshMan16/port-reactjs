c20337e50b49b7a2a65825c65bcb5a839869050d
NikeshMan16 <nikeshmanshrestha@gmail.com>
2025-06-07 15:05:06 +0545

    change in data

--- src/data/data.js
+++ src/data/data.js
@@ -1,22 +1,11 @@
-import WorkIm from '../assets/project/workImg.jpeg'
-import RealEst from '../assets/project/realestate.jpg'
+import apiTest from '../assets/project/postmanProject.png'
 import workGen from '../assets/project/workoutgen.png'
 import fakeNews from '../assets/fakenews.jpg'
 import resumeMatcher from '../assets/project/Resume.webp'
-import shopCart from '../assets/project/shopcart.webp'
 import autoScript from '../assets/project/autotest.png'
 import performTest from '../assets/project/PerformTest.png'
 
 export const data=[
-    {
-        id:1,
-        name:"Autotesting Ecommerce site",
-        image:autoScript,
-        tags: ["Selenium", "Python", "QA automation"],
-        github:"https://github.com/NikeshMan16/ProjectAutomation",
-        live:"",
-    },
-
     {
         id:2,
         name:"Performance Testing RESTful API using Jmeter",
@@ -27,19 +16,30 @@ export const data=[
     },
    
     {
+        id:1,
+        name:"Autotesting Ecommerce site",
+        image:autoScript,
+        tags: ["Selenium", "Python", "QA automation"],
+        github:"https://github.com/NikeshMan16/ProjectAutomation",
+        live:"",
+    },
+
+    
+    
+    {
+        id:4,
+        name:"Platzi API testing using Postman",
+        image:apiTest,
+        tags: ["API testing", "Automation", "QA"],
+        github:"https://github.com/NikeshMan16/test_cart_functionality",
+        live:"",
+    },
+    {
         id:3,
         name:"Fake News Detection system",
         image:fakeNews,
         tags: ["Python", "AI", "Machine Learning"],
         github:"https://github.com/NikeshMan16/Major-fake-news-",
-        live:"",
-    },
-    {
-        id:4,
-        name:"ShopCart Application",
-        image:shopCart,
-        tags: ["React", "Frontend", "E-commerce"],
-        github:"https://github.com/NikeshMan16/test_cart_functionality",
         live:"",
     },
     {
